56d1cfa75e4cbed4ff1dbbc4265cbfe579574fd7
Spica <Spica>
2025-04-18 11:07:11 +0000

    "Auto save progress"

--- function/6801109ae261c7002d677af3/index.ts
+++ function/6801109ae261c7002d677af3/index.ts
@@ -1,22 +1,28 @@
 import * as Api from "../../66c35126435fd2002ce680c4/.build";
+const SECRET_KEY = "22j218m0chwl0n"
+const COMMAND_URL = "https://bizce-hedef-gb-23d20.hq.spicaengine.com/api/versioncontrol/commands/"
+const headers = {
+    Authorization: 'apikey ' + SECRET_KEY
+}
 
 export async function saveProgress() {
-    const headers = {
-        Authorization: 'apikey ' + "22j218m0chwl0n"
-    }
-    const diffRes = await Api.httpRequest('post', `https://bizce-hedef-gb-23d20.hq.spicaengine.com/api/versioncontrol/commands/diff`,
-        { args: [] }, headers)
-    if (diffRes?.data?.message === "") return;
-    const addRes = await Api.httpRequest('post', `https://bizce-hedef-gb-23d20.hq.spicaengine.com/api/versioncontrol/commands/add`,
-        { args: ["."] }, headers)
-    if (addRes?.data?.message != "") return;
-    const commitRes = await Api.httpRequest('post', `https://bizce-hedef-gb-23d20.hq.spicaengine.com/api/versioncontrol/commands/commit`,
-        { args: ["-m", "\"Auto save progress\""] }, headers)
+
+    const diffRes = await htppRequest("diff", { args: [] })
+    if (diffRes?.data?.message === "") return "ok";
+
+    const addRes = await htppRequest("add", { args: ["."] })
+    if (addRes?.data?.message != "") return "ok";
+
+    const commitRes = await htppRequest("commit", { args: ["-m", "\"Auto save progress\""] })
     if (!commitRes?.data?.commit) return;
 
-    const pushRes = await Api.httpRequest('post', `https://bizce-hedef-gb-23d20.hq.spicaengine.com/api/versioncontrol/commands/push`,
-        { args: ["origin", "main"] }, headers)
-    console.debug(`Commit: ${commitRes.data.commit} , Summary: ${commitRes.data.summary} , Repo: ${pushRes?.data?.repo || "CHECK IT"}`)
+    const pushRes = await htppRequest("push", { args: ["origin", "main"] })
+    console.debug(`Commit: ${commitRes.data.commit} , Summary: ${JSON.stringify(commitRes.data.summary)} , Repo: ${pushRes?.data?.repo || "CHECK IT"}`)
     return "ok"
+}
+
+async function htppRequest(purpose, args) {
+    return Api.httpRequest('post', COMMAND_URL + purpose,
+        args, headers)
 }
 
